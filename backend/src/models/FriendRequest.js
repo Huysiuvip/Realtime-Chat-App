dd358be1f5cd7592b783e194da1b07acc8f3e766
Huysiuvip <huy281024@gmail.com>
2026-04-17 21:53:54 +0700

    create model chat_app

---
+++ backend/src/models/FriendRequest.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+
+const friendRequestSchema = new mongoose.Schema({
+    from:{
+        type : mongoose.Schema.Types.ObjectId,
+        ref : 'User',
+        required : true
+    },
+    to:{
+        type : mongoose.Schema.Types.ObjectId,
+        ref : 'User',
+        required : true
+    },
+    massage:{
+        type : String,
+        maxlength : 300,
+    }
+
+}, { timestamps: true });
+
+friendRequestSchema.index({from: 1, to: 1}, {unique : true});
+
+//  tối ưu truy vấn
+friendRequestSchema.index({from: 1});
+friendRequestSchema.index({to: 1});
+
+const FriendRequest = mongoose.model('FriendRequest', friendRequestSchema);
+
+export default FriendRequest;
